d21eaa2af3790dde7c20b36b06f969fe598a61bb
JR Tashjian <jrtashjian@gmail.com>
2022-09-26 23:47:57 -0400

    Update local build-and-release script

--- build-and-release.sh
+++ build-and-release.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-docker build --no-cache -t "ghcr.io/jrtashjian/cmangos-builder:latest" ./builder
-docker push "ghcr.io/jrtashjian/cmangos-builder:latest"
-
 variants=(classic tbc wotlk)
 images=(extractors realmd mangosd)
 
 for variant in "${variants[@]}"; do
 	for image in "${images[@]}"; do
-		docker build --no-cache -t "ghcr.io/jrtashjian/cmangos-$image-$variant:latest" ./$image --build-arg CMANGOS_CORE=$variant
-		docker push "ghcr.io/jrtashjian/cmangos-$image-$variant:latest"
+		docker build --no-cache -t "ghcr.io/jrtashjian/cmangos-docker/$image-$variant:master" ./$image --build-arg CMANGOS_CORE=$variant
+		docker push "ghcr.io/jrtashjian/cmangos-docker/$image-$variant:master"
 	done
 done
