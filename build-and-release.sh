29be79c0b0ba08aa5ce83c4dfefc74d4f1590ac4
JR Tashjian <jrtashjian@gmail.com>
2022-09-30 01:28:46 -0400

    Update docker-compose examples

--- build-and-release.sh
+++ build-and-release.sh
@@ -5,7 +5,7 @@ images=(extractors realmd mangosd)
 
 for variant in "${variants[@]}"; do
 	for image in "${images[@]}"; do
-		docker build --no-cache -t "ghcr.io/jrtashjian/cmangos-docker/$image-$variant:master" ./$image --build-arg CMANGOS_CORE=$variant
-		docker push "ghcr.io/jrtashjian/cmangos-docker/$image-$variant:master"
+		docker build --no-cache -t "ghcr.io/jrtashjian/cmangos-$image-$variant:latest" ./$image --build-arg CMANGOS_CORE=$variant
+		docker push "ghcr.io/jrtashjian/cmangos-$image-$variant:latest"
 	done
 done
