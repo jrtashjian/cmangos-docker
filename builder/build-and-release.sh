e68b59828d8edbd71e85a3baa5adacb8d4b6a694
JR Tashjian <jrtashjian@gmail.com>
2022-09-30 01:33:20 -0400

    Remove cmangos-docker from image name (#10)

--- builder/build-and-release.sh
+++ builder/build-and-release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build --no-cache -t "ghcr.io/jrtashjian/cmangos-docker/builder-base:latest" .
-docker push "ghcr.io/jrtashjian/cmangos-docker/builder-base:latest"
+docker build --no-cache -t "ghcr.io/jrtashjian/cmangos-builder-base:latest" .
+docker push "ghcr.io/jrtashjian/cmangos-builder-base:latest"
