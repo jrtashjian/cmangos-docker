dd14be0f9310a1cb47a3d4394c63294d28f55359
JR Tashjian <hello@jrtashjian.com>
2025-05-17 14:09:05 -0400

    Make build source and registry dynamic

--- base-image/build-and-release.sh
+++ base-image/build-and-release.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-docker build -t "registry.int.jrtashjian.com/jrtashjian/cmangos-docker/base-image" .
-docker push "registry.int.jrtashjian.com/jrtashjian/cmangos-docker/base-image"
+IMAGE_SOURCE="${IMAGE_SOURCE:-https://gitlab.int.jrtashjian.com/jrtashjian/cmangos-docker}"
+
+REGISTRY="${REGISTRY:-registry.int.jrtashjian.com}"
+DATE_TAG=$(date +%Y.%m.%d)
+
+docker build . --no-cache \
+    --label "org.opencontainers.image.source=${IMAGE_SOURCE}" \
+    -t "${REGISTRY}/jrtashjian/cmangos-docker/builder-base:latest" \
+    -t "${REGISTRY}/jrtashjian/cmangos-docker/builder-base:${DATE_TAG}"
+
+docker push "${REGISTRY}/jrtashjian/cmangos-docker/builder-base:latest"
+docker push "${REGISTRY}/jrtashjian/cmangos-docker/builder-base:${DATE_TAG}"
